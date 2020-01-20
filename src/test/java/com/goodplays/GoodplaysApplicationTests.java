d63cb13c517e615b09c1d6105826d7d201f3b32b
Gregory Cowart <glc690@gmail.com>
2020-01-20 10:57:34 -0500

    code upload

---
+++ src/test/java/com/goodplays/GoodplaysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.goodplays;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoodplaysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
